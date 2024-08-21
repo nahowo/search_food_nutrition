3550703ac5bc4b29fc9fd4793157dc85e57bf57c
nahowo <nahowo@naver.com>
2024-08-21 14:03:44 +0900

    initial commit

---
+++ src/main/java/nahowo/example/FoodNut/FoodNutApplication.java
@@ -0,0 +1,13 @@
+package nahowo.example.FoodNut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodNutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodNutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nahowo/example/FoodNut/FoodNutApplicationTests.java
@@ -0,0 +1,13 @@
+package nahowo.example.FoodNut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodNutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
