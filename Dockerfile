e68412ca03fec40ab6e2c5d99116a9d9c76fe7c4
nahowo <nahowo@naver.com>
2024-10-08 16:55:10 +0900

    set: 도커, MySQL, ES 세팅

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM ubuntu:latest
+LABEL authors="nahyun"
+
+ENTRYPOINT ["top", "-b"]
--- src/main/java/nahowo/example/FoodNut/FoodNutApplication.java
+++ src/main/java/nahowo/example/FoodNut/FoodNutApplication.java
@@ -2,7 +2,9 @@ package nahowo.example.FoodNut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource("classpath:env.properties")
 @SpringBootApplication
 public class FoodNutApplication {
 
